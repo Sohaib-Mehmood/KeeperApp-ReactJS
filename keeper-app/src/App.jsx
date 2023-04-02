f830807929ec3c0dabe7c278ebc3d1b785c245d7
Sohaib-Mehmood <sohaibmehmood776@gmail.com>
2023-04-02 22:49:39 +0500

    added Note functionality

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -4,13 +4,18 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../src/notes"
 
-console.log(notes);
-
 
 function App() {
   return (
     <div>
       <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
